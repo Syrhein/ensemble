f392012ae12e5f46c30332ea9f958e7f9202ec71
Syrhein <namhib@naver.com>
2025-01-08 16:46:08 +0900

    Merge branch 'main' of https://github.com/SCNULMH/Ensemble.git into main

--- test1/src/main/java/smhrd/controller/MainCon.java
+++ test1/src/main/java/smhrd/controller/MainCon.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @WebServlet("/MainCon")
 public class MainCon extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
